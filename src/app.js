ddeccced95469880052b69ffca2cc6f26b147d60
ewhuet <ewhuet@gmail.com>
2024-04-25 21:37:44 +0300

    cleaned up the app.js file to remove unused lines

--- src/app.js
+++ src/app.js
@@ -4,35 +4,21 @@ import LinkedList from "./LinkedList";
 const newHashMap = new HashMap(16);
 
 newHashMap.set("1234", "dad")
-newHashMap.set("12345", "ewan")
-newHashMap.set("12334", "mum")
-newHashMap.set("123456", "daisy")
-newHashMap.set("1234567", "dexter")
+newHashMap.set("12345", "buddy")
+newHashMap.set("12334", "elf")
+newHashMap.set("123456", "santa")
+newHashMap.set("1234567", "mom")
 newHashMap.get("1234")
 
 
-newHashMap.set("123456901234", "colleen")
-// newHashMap.display()
+newHashMap.set("123456901234", "rudolph")
 newHashMap.get("123456")
 newHashMap.has("1234")
 newHashMap.has("12345")
-// newHashMap.delete("123456")
 newHashMap.display()
 console.log(newHashMap.length())
-// newHashMap.clear();
-// newHashMap.set("123456901234", "colleen")
-// newHashMap.display()
 newHashMap.set("093476264", "pip")
 newHashMap.display()
 console.log(newHashMap.keys())
 console.log(newHashMap.values())
 console.log(newHashMap.entries())
-
-
-
-
-
-// const ll = new LinkedList();
-// ll.append("test", 11)
-// console.log(ll)
-// console.log(ll.toString())
